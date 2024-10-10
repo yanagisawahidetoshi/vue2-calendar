c8d4b2b46132a599868aa640d5bb032d92ff3812
oi.tomoharu <oi.tomoharu@raccoon.ne.jp>
2024-10-10 14:35:39 +0900

    20241010 1~31の配列を用意

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <ol>
+      <li v-for="date in dates" :key="date">
+        <p>{{ date }}</p>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   components: {},
+  data(){
+    return {
+      dates:[...Array(31)].map((_, i) => i + 1)
+    }
+  },
 };
 </script>
