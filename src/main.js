5cc58c6936f2d25d761231ffb3b1c166259fda9c
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2024-10-13 16:18:41 +0900

    VUeモーダル読み込み

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import "destyle.css";
 
+import Modal from "@burhanahmeed/vue-modal-2";
+
+Vue.use(Modal);
+
 Vue.config.productionTip = false;
 
 new Vue({
