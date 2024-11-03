ad1ff52a0e587c2fa620e8d42ce6db7737df1512
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2024-11-03 21:09:45 +0900

    composition api導入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "destyle.css";
+import VueCompositionAPI from "@vue/composition-api";
 
 import Modal from "@burhanahmeed/vue-modal-2";
 
 Vue.use(Modal);
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
